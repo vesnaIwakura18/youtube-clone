ed03547499244b1b1b74bf47d91d42c6a49d57ab
sacrxsanct <sacrxsanct@outlook.com>
2023-12-02 20:23:42 +0600

    add video-processing-service

---
+++ video-processing-service/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+
+const app = express();
+app.use(express.json());
+
+app.post("/process-video", (req, res) => {
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing file path.");
+    }
+
+    ffmpeg(inputFilePath)
+        .outputOptions("-vf", "scale=-1:360") // 360p
+        .on("end", () => {
+            res.status(200).send('Processing finished successfully.');
+        })
+        .on("error", (err) => {
+            console.log(`An error occurred: ${err.message}`);
+            res.status(500).send(`Internal Server Error: ${err.message}`);
+        })
+        .save(outputFilePath);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(
+        `Video processing service listening at http://localhost:${port}`);
+});
