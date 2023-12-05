e3f81f4c58bf60cd702cb80e8baea69f2f05ef21
sacrx <sacrxsanct@outlook.com>
2023-12-05 16:35:44 +0600

    introduce front-end

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { 
+    getAuth, 
+    signInWithPopup, 
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    User
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBN27igWWbOd-LgKMb2chzvFImC-fIV2sk",
+  authDomain: "yt-clone-c3051.firebaseapp.com",
+  projectId: "yt-clone-c3051",
+  appId: "1:522051628741:web:940ba98157abca9fe54aac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider())
+}
+
+export function signOut() {
+    return auth.signOut();
+}
+
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback);
+}
