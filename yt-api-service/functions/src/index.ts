2ea032f2cb35b1ee541f0e5a1e68f67609bd01e3
sacrx <sacrxsanct@outlook.com>
2023-12-07 12:43:10 +0600

    add getVideos firebase function

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -12,6 +12,8 @@ const storage = new Storage();
 
 const rawVideoBucketName = "iwak-yt-raw-videos";
 
+const videoCollectionId = "videos";
+
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
     uid: user.uid,
@@ -45,4 +47,10 @@ export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
   });
 
   return {url, fileName};
+});
+
+export const getVideos = onCall({maxInstances: 1}, async () => {
+  const snapshot =
+    await firestore.collection(videoCollectionId).limit(10).get();
+  return snapshot.docs.map((doc) => doc.data());
 });
