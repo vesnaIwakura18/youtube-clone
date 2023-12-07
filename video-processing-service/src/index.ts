a0c4dd8b5ec7643ff0fd355b09de533f557fc9ac
sacrx <sacrxsanct@outlook.com>
2023-12-07 12:18:31 +0600

    add status handling

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { convertVideo, deleteProceseedVideo, deleteRawVideo, setupDirectories, uploadProcessedVideo } from "./storage";
 import {downloadRawVideo} from "./storage";
+import { isVideoNew, setVideo } from "./firestore";
 
 setupDirectories();
 
@@ -22,6 +23,17 @@ app.post("/process-video", async (req, res) => {
 
     const inputFileName = data.name;
     const outputFileName = `processed-${inputFileName}`;
+    const videoId = inputFileName.split('.')[0];
+
+    if (!isVideoNew(videoId)) {
+        return res.status(400).send('Bad Request: video already processing or processed')
+    } else {
+        await setVideo(videoId, {
+            id: videoId,
+            uid: videoId.split('-')[0],
+            status: 'processing'
+        });
+    }
 
     await downloadRawVideo(inputFileName);
 
@@ -38,6 +50,11 @@ app.post("/process-video", async (req, res) => {
     }
 
     await uploadProcessedVideo(outputFileName);
+
+    await setVideo(videoId, {
+        status: 'processed',
+        filename: outputFileName
+    });
 
     await Promise.all([
         deleteRawVideo(inputFileName),
